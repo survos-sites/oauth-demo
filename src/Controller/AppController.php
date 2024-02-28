af170ad48504d2fd4041f2df1f92e63351656c83
Tac Tacelosky <tacman@gmail.com>
2024-02-28 11:01:25 -0600

    add production url for easier config

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -4,19 +4,27 @@ namespace App\Controller;
 
 use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class AppController extends AbstractController
 {
     #[Route('/', name: 'app_app')]
-    public function index(GoogleController $googleController, ClientRegistry $clientRegistry): Response
+    public function index(Request $request, GoogleController $googleController, ClientRegistry $clientRegistry): Response
     {
 
         foreach (['google', 'github'] as $service) {
             $redirect = $googleController->getRedirect($service);
             parse_str($queryString = parse_url($targetUrl = $redirect->getTargetUrl(), PHP_URL_QUERY), $array);
+            $redirectUri = $array['redirect_uri'];
+            $productionRedirctUri = str_replace($request->getSchemeAndHttpHost(),
+                $this->getParameter('production_url'),
+                $redirectUri
+            );
             $services[$service] = [
+                'redirectUri' => $redirectUri,
+                'productionRedirectUri' => $productionRedirctUri,
                 'targetUrl' => $redirect->getTargetUrl(),
                 'targetInfo' => parse_url($redirect->getTargetUrl()),
                 'query' => $array,
@@ -29,6 +37,7 @@ class AppController extends AbstractController
             ];
         }
         return $this->render('app/index.html.twig', [
+            'productionUrl' => $this->getParameter('production_url'),
             'services' => $services,
             'projectId' => $this->getParameter('google_project_id'),
         ]);
